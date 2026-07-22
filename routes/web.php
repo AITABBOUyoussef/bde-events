e9a8d5a06c400b090a9f9ce6d09c7828832e824b
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-07-22 13:07:46 +0100

    Implement admin role check in middleware and update user model to include role

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+
+// Route::middleware(['auth' , 'is_admin'])->prefix('admin')->group(function (){
+// Route::get('/admin/dashboard', function () {
+//     return 'sddddddddd';
+// })->name('dashboard_admin');
+
+// });
+
+Route::middleware(['auth' , 'is_admin'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
