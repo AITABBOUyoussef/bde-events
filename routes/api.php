afad2fd76b3157303b22d9539c457f7743c67258
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-08-07 19:42:49 +0100

    Implement authentication with login functionality and user token generation

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::get('/index', [EventController::class, 'index']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+     Route::get('/events', [EventController::class, 'index']);
+
+});
 
